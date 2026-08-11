2901ba0360baf8aa9ed471f2e4dbbcb6ffabf296
reallaksh19 <reallaksh19@gmail.com>
2026-08-11 18:29:25 +0400

    style(lfea): include semantic Compare styles

--- src/lfea/main.js
+++ src/lfea/main.js
@@ -2,6 +2,7 @@ import '../workspace/linear-piping-results-workbench.css';
 import './standalone.css';
 import './native-results.css';
 import './native-history.css';
+import './native-comparison.css';
 import { bootstrapLfeaStandalone } from './bootstrap.js';
 
 const applicationRoot = document.getElementById('root');

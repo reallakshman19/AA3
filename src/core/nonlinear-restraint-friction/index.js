d6ced32f78a891d5da36d037233817a0d821ead9
reallaksh19 <reallaksh19@gmail.com>
2026-08-12 06:42:17 +0400

    M047 friction: export stiffness unit contract

--- src/core/nonlinear-restraint-friction/index.js
+++ src/core/nonlinear-restraint-friction/index.js
@@ -14,3 +14,8 @@ export {
   FRICTION_EXECUTION_READINESS_BLOCKERS,
   assessFrictionExecutionReadiness,
 } from './friction-execution-readiness.js';
+export {
+  parseCaesarInputXmlForceLengthUnits,
+  normalizeForcePerLengthToNPerM,
+  normalizeDisplayedCaesarFrictionStiffnessToSI,
+} from './caesar-friction-unit-contract.js';

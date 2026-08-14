449ead62cbadf6925d10ed93d799396f7e39e21a
reallaksh19 <reallaksh19@gmail.com>
2026-08-14 22:45:19 +0400

    feat(lafea3): expose formulation qualification guard

--- src/core/local-continuum/index.js
+++ src/core/local-continuum/index.js
@@ -1,4 +1,9 @@
-export { BASE_LIMITATIONS, CANONICAL_UNITS, DOFS, ENGINEERING_LEVEL, FORMULA_IDS, FORMULATIONS, MODEL_SCHEMA, QUALIFICATION_PROFILE, QUALIFICATION_PROFILE_SCHEMA, QUALIFICATION_STATES, RESULT_SCHEMA, SOURCE_EVIDENCE_SCHEMA } from './constants.js';
+export {
+  BASE_LIMITATIONS, CANONICAL_UNITS, DOFS, ENGINEERING_LEVEL, FORMULA_IDS,
+  FORMULATION_GUARDS, FORMULATIONS, MODEL_SCHEMA, QUALIFICATION_PROFILE,
+  QUALIFICATION_PROFILE_SCHEMA, QUALIFICATION_STATES, RESULT_SCHEMA,
+  SOURCE_EVIDENCE_SCHEMA,
+} from './constants.js';
 export { createCanonicalLocalContinuumModel, validateCanonicalLocalContinuumModel } from './canonical-model.js';
 export { calculateLocalContinuum, reconstructContinuumResultHashes } from './calculate.js';
 export { constitutiveEvidence } from './constitutive.js';

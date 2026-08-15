4ad9904fab70bb51e5d4040a5a00156e41bc0110
reallaksh19 <reallaksh19@gmail.com>
2026-08-15 09:29:08 +0400

    export empirical flexibility kernel

--- src/core/empirical-piping-mechanics/index.js
+++ src/core/empirical-piping-mechanics/index.js
@@ -21,6 +21,11 @@ export {
   compileInitialStrainLoad,
 } from './member.js';
 export { compileSegmentedPlanarElbow } from './elbow.js';
+export {
+  assembleUnitForceFlexibilityMatrix,
+  calculatePrismaticVirtualWorkContribution,
+  integrateLinearEndFieldProduct,
+} from './flexibility.js';
 export { solveScaledDenseSystem } from './linear-system.js';
 export { assemblePlanarSystem, solveAssembledPlanarSystem, solvePlanarSystem } from './assembly.js';
 export { solveUnilateralActiveSet } from './contact.js';

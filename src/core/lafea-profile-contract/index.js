da415c2cb9e42edf888a280d52325c315d7a594d
reallaksh19 <reallaksh19@gmail.com>
2026-08-15 05:10:36 +0400

    Export stage-qualified LAFEA policies

--- src/core/lafea-profile-contract/index.js
+++ src/core/lafea-profile-contract/index.js
@@ -2,5 +2,6 @@ export * from './constants.js';
 export * from './errors.js';
 export * from './schema.js';
 export * from './profile-registry.js';
+export * from './stage-qualified-policies.js';
 export * from './hash-lineage.js';
 export * from './approximation-register.js';

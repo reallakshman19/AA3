bb8ac37ff09d1ad5399d0623f8afc413588c70bc
reallaksh19 <reallaksh19@gmail.com>
2026-08-17 11:36:11 +0400

    LAFEA.4 TECH-12B: expose retained parent-normal companion custody API

--- src/workspace/lafea-workbench-orchestrator-api.js
+++ src/workspace/lafea-workbench-orchestrator-api.js
@@ -153,6 +153,12 @@ export function createLafeaWorkbenchOrchestratorApi(context) {
       c.meshGeneration.selectMeshProfile(stageId),
     selectRetainedAnalysisMeshEvidenceV2: (stageId = activeStageId()) =>
       c.meshGeneration.selectEvidence(stageId),
+    selectRetainedAnalysisMeshParentNormalCompanion:
+      c.selectRetainedAnalysisMeshParentNormalCompanion,
+    exportRetainedAnalysisMeshParentNormalCompanion:
+      c.exportRetainedAnalysisMeshParentNormalCompanion,
+    validateRetainedAnalysisMeshParentNormalCompanion:
+      c.validateRetainedAnalysisMeshParentNormalCompanion,
     selectAnalysisMeshPlan: (stageId = activeStageId()) =>
       c.meshGeneration.selectPlan(stageId),
     buildOrchestrationProjection: (stageId = activeStageId()) =>

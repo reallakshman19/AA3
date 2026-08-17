2f3a64216e44dc56a81fe8707caf946cfbbf5130
reallaksh19 <reallaksh19@gmail.com>
2026-08-17 06:54:01 +0400

    Export empirical correlation core

---
+++ src/core/local-attachment-correlation/index.js
@@ -0,0 +1,19 @@
+export {
+  BASE_LIMITATIONS,
+  CORRELATION_PROFILE_SCHEMA,
+  CORRELATION_REQUEST_SCHEMA,
+  CORRELATION_RESULT_SCHEMA,
+  FORCE_COMPONENTS,
+  INTERPOLATION_POLICIES,
+  LOAD_BASES,
+  LOAD_COMPONENTS,
+  MOMENT_COMPONENTS,
+  QUALIFICATION_STATES,
+  STRESS_CLASSES,
+  STRESS_COMPONENTS,
+  SURFACES,
+} from './constants.js';
+export { calculateLocalAttachmentCorrelation } from './calculate.js';
+export { bilinearNoExtrapolation, bracket } from './interpolation.js';
+export { correlationDatasetHash, createCorrelationProfile } from './profile.js';
+export { syntheticCorrelationProfile, syntheticCorrelationRequest } from './synthetic-profile.js';

c033c64dda5a7abad87f6f245be4d1515da445a8
reallaksh19 <reallaksh19@gmail.com>
2026-08-27 20:41:15 +0530

    PR1493: correct one-click view HTML escaping

--- src/workspace/load-calc-current-system-view.js
+++ src/workspace/load-calc-current-system-view.js
@@ -334,8 +334,8 @@ function emptyArray(value) {
 }
 
 function escapeHtml(value) {
-  return String(value ?? '').replace(/[&<>\"]/g, (character) => ({
-    '&': '&amp;', '<': '&lt;', '>': '&gt;', '\"': '&quot;',
+  return String(value ?? '').replace(/[&<>"]/g, (character) => ({
+    '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;',
   })[character]);
 }
 
